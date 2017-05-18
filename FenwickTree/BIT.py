cdd13cae085da89a847c5b6266a359e365ed1d8a
ShihFengLin <noahleft@gmail.com>
2017-05-18 14:02:56 +0800

    add Fenwick Tree update and sum func

--- FenwickTree/BIT.py
+++ FenwickTree/BIT.py
@@ -1,7 +1,32 @@
 #!/usr/local/bin/python3
 
+fenwick_index = lambda x: x+1
+update_parent_index  = lambda x: x + (x & -x)
+sum_parent_index = lambda x: x - (x & -x)
 
 class Fenwick1D(object):
     def __init__(self,data_array):
         self.data = data_array
-        print('Hello World')
+        self.create_fenwick_tree()
+    def create_fenwick_tree(self):
+        self.fenwick_tree = [0 for x in range(len(self.data)+1)]
+        for index,item in enumerate(self.data):
+            self.update(index, item)
+    def update(self, index, value):
+        new_index = fenwick_index(index)
+        while new_index<=len(self.data):
+            self.fenwick_tree[new_index] += value
+            new_index = update_parent_index(new_index)
+    def calculate_first_n(self,n):
+        ret_sum = 0
+        new_index = n
+        while new_index > 0:
+            ret_sum += self.fenwick_tree[new_index]
+            new_index = sum_parent_index(new_index)
+        return ret_sum
+    def check(self,n):
+        if sum(self.data[:n]) != self.calculate_first_n(n):
+            print(sum(self.data[:n]),'diff ',self.calculate_first_n(n))
+            return False
+        else:
+            return True
--- script.py
+++ script.py
@@ -3,6 +3,12 @@
 from FenwickTree.BIT import Fenwick1D
 import random
 
-base_array = [random.randint(0,100) for x in range(25)]
+n = 1000
+
+base_array = [random.randint(0,100) for x in range(n)]
 
 fenwick_tree_1d = Fenwick1D(base_array)
+
+
+if all([fenwick_tree_1d.check(item) for item in range(n)]):
+    print('all pass')
